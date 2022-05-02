dc8c865b1131e1a1be09214a739b754fe25fa895
hyparh <hyparh@gmail.com>
2022-05-02 19:48:24 +0300

    Added favicon

---
+++ PlantShop/Models/Cart.cs
@@ -0,0 +1,20 @@
+﻿using PlantShop.Models.Plants;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PlantShop.Models
+{
+    public class Cart
+    {
+        public int Id { get; init; }
+
+
+        [ForeignKey(nameof(User))]
+        public string UserId { get; set; }
+        public User User { get; set; }
+
+
+        [ForeignKey(nameof(Plant))]
+        public string PlantId { get; set; }
+        public Plant Plant { get; set; }
+    }
+}
